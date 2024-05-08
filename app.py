14cff3386b88728219b17fa444f7db2033cea199
Alisson <alissonjscosta@gmail.com>
2024-05-08 02:07:36 -0300

    update

--- app.py
+++ app.py
@@ -1,22 +1,112 @@
+# from flask import Flask, request, jsonify, abort, session
+# from flask_session import Session
+# from flask_cors import CORS
+# import os
+# from transformers import pipeline
+# import PyPDF2
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'hdfgsdfgsçknms.jbsdfssdx'
+# app.config['SESSION_TYPE'] = 'filesystem'
+# Session(app)
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+# CORS(app, supports_credentials=True)
+
+# # Configuração do modelo Hugging Face
+# model_id = "meta-llama/Meta-Llama-3-8B"
+# token = "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"
+# chat_model = pipeline("text-generation", model=model_id, use_auth_token=token)
+
+# @app.route('/api/chat/llama3', methods=['POST'])
+# def chat():
+#     if 'history' not in session:
+#         session['history'] = []
+
+#     user_message = request.form.get('message', '')
+#     file = request.files.get('file')
+
+#     if file and allowed_file(file.filename):
+#         pdf_text = extract_text_from_pdf(file)
+#         user_message += f" PDF Text: {pdf_text}"
+
+#     if user_message.strip():
+#         session['history'].append(f"User: {user_message}")
+
+#     full_context = "\n".join(entry.split(": ", 1)[1] for entry in session['history'])
+
+#     response = chat_model(full_context, max_length=512, num_return_sequences=1)[0]
+
+#     session['history'].append(f"Bot: {response['generated_text']}")
+
+#     return jsonify({'message': response['generated_text']})
+
+# def extract_text_from_pdf(file):
+#     try:
+#         pdf_reader = PyPDF2.PdfReader(file.stream)
+#         return ''.join(page.extract_text() or "" for page in pdf_reader.pages)
+#     except Exception as e:
+#         print(f"Failed to extract PDF text: {e}")
+#         abort(400, 'Failed to extract text from PDF.')
+
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'pdf'}
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=8501)
+
 from flask import Flask, request, jsonify, abort, session
 from flask_session import Session
 from flask_cors import CORS
 import os
-from transformers import pipeline
+import google.generativeai as genai
 import PyPDF2
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hdfgsdfgsçknms.jbsdfssdx'
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['SECRET_KEY'] = 'gdçkmdf~çpgndiaÕBNLKFJHDBLS56461651dfgsd3g4'
+app.config['SESSION_TYPE'] = 'filesystem' 
+Session(app)  
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 CORS(app, supports_credentials=True)
 
-# Configuração do modelo Hugging Face
-model_id = "meta-llama/Meta-Llama-3-8B"
-token = "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"
-chat_model = pipeline("text-generation", model=model_id, use_auth_token=token)
+os.environ['GOOGLE_API_KEY'] = "AIzaSyBcYei2m_mC8QSBQh72DQ1SbbwOL_pNHfo"
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+#model = genai.GenerativeModel('gemini-1.5-pro-latest')  # Atualizado para o modelo "gemini 1.5 Pro"
+
+# Set up the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                             generation_config=generation_config,
+                             safety_settings=safety_settings,
+                              
+                              )
 
 @app.route('/api/chat', methods=['POST'])
 def chat():
@@ -28,18 +118,18 @@ def chat():
 
     if file and allowed_file(file.filename):
         pdf_text = extract_text_from_pdf(file)
-        user_message += f" PDF Text: {pdf_text}"
+        user_message += f"\nPDF Text: {pdf_text}"
 
     if user_message.strip():
         session['history'].append(f"User: {user_message}")
 
     full_context = "\n".join(entry.split(": ", 1)[1] for entry in session['history'])
 
-    response = chat_model(full_context, max_length=512, num_return_sequences=1)[0]
+    response = model.generate_content(full_context)
 
-    session['history'].append(f"Bot: {response['generated_text']}")
+    session['history'].append(f"Bot: {response.text}")
 
-    return jsonify({'message': response['generated_text']})
+    return jsonify({'message': response.text})
 
 def extract_text_from_pdf(file):
     try:
@@ -54,3 +144,4 @@ def allowed_file(filename):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8501)
+
